c6a658fa819608218c9227bf9cfbb5deaa546d37
surendra kumar <surendrakeerbaniya@gmail.com>
2024-07-03 22:36:56 +0530

    tailwind css added

--- routes/index.js
+++ routes/index.js
@@ -5,22 +5,30 @@ const { sendMail } = require('../utils/sendMail');
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-router.post("/", async (req,res)=>{
-  const email = await new userModel({...req.body})
-  await email.save()
-  sendMail(req,res);
-  res.send("mail send successfully");
+router.get("/", (req,res)=>{
+  res.render("index")
 })
 
 router.get("/register", (req,res,next)=>{
   res.render("register")
 })
 
-router.get("/home", (req,res)=>{
-  res.render("home")
+router.get("/login", (req,res)=>{
+  res.render("login")
 })
+
+router.get('/main', function(req, res, next) {
+  res.render('main', { title: 'Express' });
+});
+
+router.post("/main", async (req,res)=>{
+  const email = await new userModel({...req.body})
+  await email.save()
+  sendMail(req,res);
+  res.send("mail send successfully");
+})
+
+
+
 
 module.exports = router;
